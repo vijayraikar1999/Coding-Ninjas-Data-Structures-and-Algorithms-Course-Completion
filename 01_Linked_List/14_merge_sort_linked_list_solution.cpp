04e15e0b3aa799ff3d55b8373a302f95c5dbcc26
vijayraikar1999 <vijayraikar1999@gmail.com>
2024-01-07 02:49:18 +0530

    Moderate

---
+++ 01_Linked_List/14_merge_sort_linked_list_solution.cpp
@@ -0,0 +1,96 @@
+#include <cstddef>
+
+class Node{
+    public:
+        int data;
+        Node *next;
+        Node() {
+            this->data = 0;
+            next = NULL;
+        }
+        Node(int data) {
+            this->data = data;
+            this->next = NULL;
+        }
+        Node(int data, Node* next) {
+            this->data = data;
+            this->next = next;
+        }
+};
+
+Node *midPoint(Node *head)
+{
+    if (head == nullptr || head -> next == nullptr) {
+		return head;
+	}
+	Node* p = head;
+	Node* q = head -> next;
+	while (!( q == nullptr || q -> next == nullptr )) {
+		p = p -> next;
+		q = (q -> next) -> next; 
+	}
+	return p;
+}
+
+Node *mergeTwoSortedLinkedLists(Node *head1, Node *head2)
+{
+
+	if (head1 == nullptr) {
+		return head2;
+	}
+	if (head2 == nullptr) {
+		return head1;
+	}
+
+    Node* i = head1;
+	Node* j = head2;
+	Node* k = head1;
+	if (head1 -> data <= head2 -> data) {
+		k = head1;
+		i = head1 -> next;
+	} else {
+		k = head2;
+		j = j -> next;
+	}
+	Node* newHead = k;
+
+	while (i != nullptr && j != nullptr) {
+		if (i -> data <= j -> data) {
+			k -> next = i;
+			i = i -> next;
+			k = k -> next;
+		} else {
+			k -> next = j;
+			j = j -> next;
+			k = k -> next;
+		}
+	}
+
+	while (i != nullptr) {
+		k -> next = i;
+		i = i -> next;
+		k = k -> next;
+	}
+
+	while (j != nullptr) {
+		k -> next = j;
+		j = j -> next;
+		k = k -> next;
+	}
+
+	return newHead;
+}
+
+Node *mergeSort(Node *head)
+{
+	if (head == nullptr || head -> next == nullptr) {
+		return head;
+	}
+
+	Node* mid = midPoint(head);
+	Node* mid_next = mid -> next;
+	mid -> next = nullptr;
+	Node* left = mergeSort(head);
+	Node* right = mergeSort(mid_next);
+	return mergeTwoSortedLinkedLists(left, right);
+}
